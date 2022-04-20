ea8f469d991191dbdc341f2787046e39ed15508f
retrouser955 <htoomyataung382@gmail.com>
2022-04-20 19:24:53 +0630

    💀I forgot to unignore test files

---
+++ test/index.test.js
@@ -0,0 +1,27 @@
+const { PooPooAPI, version } = require('../index.js')
+const api = new PooPooAPI({
+    logError: true,
+    logAction: true
+})
+const apiTest = async () => {
+    // console.log(version)
+    // let pass = await api.passwordGenerator()
+    // let apiQotd = await api.qotd()
+    // let url = await api.isValidUrl('httpsm')
+    // console.log(pass)
+    // console.log(url)
+    // console.log(apiQotd)
+    // const github = await api.githubUserData('frostzzone')
+    // console.log(github)
+    // const binary = await api.binaryEncoder('Hello World!')
+    // console.log(binary)
+    // const decodedText = await api.binaryDecoder('01101000 01101001')
+    // console.log(decodedText)
+    // const spoilers = await api.discordSpoilers('Hello I am a human male')
+    // console.log(spoilers)
+    // const emoji = await api.emojiRemover('cjidoalcnoaskl🛣')
+    // console.log(emoji)
+    const ytSearch = await api.youtubeSearch('MEMORIES! 347aidian')
+    console.log(ytSearch.results[0].title)
+}
+apiTest()
